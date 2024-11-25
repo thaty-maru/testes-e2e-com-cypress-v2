5afd35cb8f0e7ece6d2250bc3042e2dba2ffddc0
Thatianne Luchsinger <thatianne.maruyama@gmail.com>
2024-11-25 16:45:23 -0400

    Lesson 2

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+	e2e: {
+		baseUrl: 'https://notes-serverless-app.com',
+		defaultCommandTimeout: 10000,
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+	},
 })
