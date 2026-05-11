1358eeeec7de3f2f5b31de432f1291363bef493e
Thatianne Maruyama Luchsinger <thatianne.maruyama@gmail.com>
2026-05-11 16:01:21 -0400

    Lesson 3

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
 	e2e: {
 		baseUrl: 'https://notes-serverless-app.com',
 		defaultCommandTimeout: 10000,
+		requestTimeout: 30000, 
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,10 @@
+describe('Login', () => {
+  it('successfully logs in', () => {
+    const emailAddress = Cypress.env('USER_EMAIL')
+        const password = Cypress.env('USER_PASSWORD')
+    cy.guiLogin(emailAddress, password)
+
+    cy.contains('h1', 'Your Notes').should('be.visible')
+    cy.contains('a', 'Create a new note').should('be.visible')
+  })
+})
