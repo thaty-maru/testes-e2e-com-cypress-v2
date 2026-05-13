a775b321739a30166fce4536d4136a8462dc6382
Thatianne Maruyama Luchsinger <thatianne.maruyama@gmail.com>
2026-05-13 10:11:01 -0400

    Configure Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,14 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+	projectId: '1aewv2',
 	e2e: {
 		baseUrl: 'https://notes-serverless-app.com',
 		env: {
 			viewportWidthBreakpoint: 768,
 		},
 		defaultCommandTimeout: 10000,
-		requestTimeout: 30000, 
-		setupNodeEvents(on, config) {
-			// implement node event listeners here
-		},
+		requestTimeout: 30000,
 		chromeWebSecurity: false,
 	},
 })
